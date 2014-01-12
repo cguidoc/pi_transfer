49d006e989d997235fdf516c7ce54ec4f3dca8fb
Chris Guidotti <guidoc@gmail.com>
2014-01-12 14:33:55 -0500

    update function

--- transfer.py
+++ transfer.py
@@ -488,18 +488,18 @@ def ShowIPAddress():
 
 def SetupGateway():
 	return
-	
+
 def ShowParameters():
 	serial_config.read(machine_serial_config)
 	parameters = []
 	try:
-		parameters.append(("Serial Baudrate\n" + serial_config.get('serial', 'baudrate')), return)
-		parameters.append(("Serial Bytesize\n" + serial_config.get('serial', 'bytesize')), return)
-		parameters.append(("Serial Stopbits\n" + serial_config.get('serial', 'stopbits')), return)
-		parameters.append(("Serial Parity\n" + serial_config.get('serial', 'parity')), return)
-		parameters.append(("Serial H.Shake\n" + serial_config.get('serial', 'xonxoff')), return)
-		parameters.append(("Machine Name\n" + serial_config.get('machine', 'machine_name')), return)
-		parameters.append(("File Extension\n" + serial_config.get('machine', 'file_extension')), return)
+		parameters.append(("Serial Baudrate\n" + serial_config.get('serial', 'baudrate')), "return")
+		parameters.append(("Serial Bytesize\n" + serial_config.get('serial', 'bytesize')), "return")
+		parameters.append(("Serial Stopbits\n" + serial_config.get('serial', 'stopbits')), "return")
+		parameters.append(("Serial Parity\n" + serial_config.get('serial', 'parity')), "return")
+		parameters.append(("Serial H.Shake\n" + serial_config.get('serial', 'xonxoff')), "return")
+		parameters.append(("Machine Name\n" + serial_config.get('machine', 'machine_name')), "return")
+		parameters.append(("File Extension\n" + serial_config.get('machine', 'file_extension')), "return")
 
 	except ConfigParser.NoSectionError as e:
 		if DEBUG:
